87437949019613875c3062f0c8b3c10fcbfedee7
gkoren2 <guy.koren@intel.com>
2022-10-18 09:52:17 +0300

    seq_example is running on Amazon_Beauty with SASRec and BERT4Rec - performance not yet verified

--- experiments/seq_example.py
+++ experiments/seq_example.py
@@ -17,21 +17,21 @@ from recbole.config import Config
 from recbole.data import create_dataset
 from recbole.data.utils import get_dataloader
 from recbole.utils import init_logger, init_seed, get_model, get_trainer, set_color
+from recbole.sampler import Sampler,RepeatableSampler
 
 
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--model', '-m', type=str, default='SASRec', help='Model for sequential rec.')
-    parser.add_argument('--dataset', '-d', type=str, default='retailrocket-sess', help='Benchmarks for sequential rec.')
+    parser.add_argument('--dataset', '-d', type=str, default='Amazon_Beauty', help='Benchmarks for sequential rec.')
     parser.add_argument('--gpu_id', '-gpu',type=int,default=0,help='gpu id [starting from 0]')
+    parser.add_argument('--batch_size_val', '-bv',type=int,default=4096,help='batch size of validation and test set')
+    parser.add_argument('--batch_size_train', '-bt',type=int,default=2048,help='batch size of training set')
     parser.add_argument('--validation', action='store_true', help='Whether evaluating on validation set (split from train set), otherwise on test set.')
     parser.add_argument('--valid_portion', type=float, default=0.1, help='ratio of validation set.')
     return parser.parse_known_args()[0]
 
-
-if __name__ == '__main__':
-    args = get_args()
-
+def build_configuration(args):
     # configurations initialization
     # for all possible parameters with their default values see recbole/properties/overall.yaml
     # some of the configurations below were inspired by recbole/properties/dataset/sample.yaml
@@ -96,12 +96,12 @@ if __name__ == '__main__':
         # 'val_interval': None,
         # 'filter_inter_by_user_or_item': True,
         'user_inter_num_interval': "[5,inf)",
-        'item_inter_num_interval': "[5,inf)",
+        # 'item_inter_num_interval': "[4,inf)",
 
         # Preprocessing
         # 'alias_of_user_id': None,
         # 'alias_of_item_id': None,
-        'alias_of_item_id': ['item_id_list'],
+        # 'alias_of_item_id': ['item_id_list'],
         # 'alias_of_entity_id': None,
         # 'alias_of_relation_id': None,
         # 'preload_weight': None,
@@ -109,7 +109,7 @@ if __name__ == '__main__':
         # 'normalize_all': None,
 
         # Benchmark .inter
-        'benchmark_filename': ['train', 'test'],
+        # 'benchmark_filename': ['train', 'test'],
 
 
         ################################
@@ -117,10 +117,10 @@ if __name__ == '__main__':
         # see default values in recbole/properties/overall.yaml
         # defaults are commented out
         # 'epochs': 300,
-        'train_batch_size': 2048,
+        'train_batch_size': args.batch_size_train,   # was 2048
         # 'learner': 'adam',
         # 'learning_rate': 0.001,
-        # 'neg_sampling': None,
+        'neg_sampling': None,
         # 'eval_step': 1,
         # 'stopping_step': 10,
         'stopping_step': 50,
@@ -140,18 +140,25 @@ if __name__ == '__main__':
             'split': {'LS': 'valid_and_test'},      # Leave-one-out splitting with valid and test splits (in addition to train)
             'order': 'TO',      # time ordered
             'mode': 'pop100'},
-        'repeatable': False,
+        'repeatable': True,     # must be True for sequential models
         'metrics': ['Hit','NDCG'],
         'topk': [1,5,10],
         'valid_metric': 'Hit@5',
         # 'valid_metric_bigger': True,
-        'eval_batch_size': 4096,
+        'eval_batch_size': args.batch_size_val,
         # 'metric_decimal_place': 4       
     }
+    config = Config(model=args.model, dataset=f'{args.dataset}', config_dict=config_dict)    
+    return config
 
-    config = Config(model=args.model, dataset=f'{args.dataset}', config_dict=config_dict)
+
+
+if __name__ == '__main__':
+    args = get_args()
+
+    config = build_configuration(args)
     init_seed(config['seed'], config['reproducibility'])
-
+ 
     # logger initialization
     init_logger(config)
     logger = getLogger()
@@ -164,26 +171,41 @@ if __name__ == '__main__':
     logger.info(dataset)
 
     # dataset splitting
-    train_dataset, test_dataset = dataset.build()
-    if args.validation:
-        train_dataset.shuffle()
-        new_train_dataset, new_test_dataset = train_dataset.split_by_ratio([1 - args.valid_portion, args.valid_portion])
-        train_data = get_dataloader(config, 'train')(config, new_train_dataset, None, shuffle=True)
-        test_data = get_dataloader(config, 'test')(config, new_test_dataset, None, shuffle=False)
-    else:
-        train_data = get_dataloader(config, 'train')(config, train_dataset, None, shuffle=True)
-        test_data = get_dataloader(config, 'test')(config, test_dataset, None, shuffle=False)
+    train_dataset,val_dataset,test_dataset = dataset.build()
+
+    train_dataloader = get_dataloader(config, 'train')
+    val_dataloader = get_dataloader(config,'validation')
+    test_dataloader = get_dataloader(config,'test')
+
+    # define the samplers
+    # sampler = RepeatableSampler(['train','validation','test'],[train_dataset,val_dataset,test_dataset],'popularity')
+    val_sampler = RepeatableSampler('validation',val_dataset,'popularity')
+    test_sampler = RepeatableSampler('test',test_dataset,'popularity')
+
+    train_data = train_dataloader(config, train_dataset, None, shuffle=True)
+    valid_data = val_dataloader(config,val_dataset,sampler = val_sampler,shuffle=False)
+    test_data = test_dataloader(config, test_dataset, sampler = test_sampler, shuffle=False)
 
     # model loading and initialization
-    model = get_model(config['model'])(config, train_data.dataset).to(config['device'])
+    model = get_model(config['model'])
+    model = model(config, train_data.dataset).to(config['device'])
     logger.info(model)
 
     # trainer loading and initialization
-    trainer = get_trainer(config['MODEL_TYPE'], config['model'])(config, model)
+    trainer = get_trainer(config['MODEL_TYPE'], config['model'])
+    trainer = trainer(config, model)
 
-    # model training and evaluation
-    test_score, test_result = trainer.fit(
-        train_data, test_data, saved=True, show_progress=config['show_progress']
+    # model training and evaluation on validation set
+    valid_score, valid_result = trainer.fit(
+        train_data, valid_data, saved=True, show_progress=config['show_progress']
     )
+
+    logger.info(set_color('validation result', 'yellow') + f': {valid_result}')
+
+    # TODO: load the model and run on the test data and report the results
+    logger.info(f'evaluating model {trainer.saved_model}')
+    
+    test_score, test_result = trainer.evaluate(
+        test_data, load_best_model=True, model_file=None, show_progress=config['show_progress'])
 
     logger.info(set_color('test result', 'yellow') + f': {test_result}')
